49c15a35db94e02be06e735eca478db8ea5992ef
Chris McGinn <cfmcginn@mit.edu>
2017-02-18 22:36:58 +0100

    Adding pidToMass class

--- include/jetGOReadHEPMC.h
+++ include/jetGOReadHEPMC.h
@@ -12,35 +12,6 @@
 
 class jetGOReadHEPMC{
  private:
-  //PID-Mass map in GeV, in order of increasing PID, mass taken from PDG 2014
-  std::map<int, double> pdgIDMassMap = 
-    {
-      //leptons
-      {11, .000510998928},
-      {12, 0.},
-      {13, .1056583715},
-      {14, 0.},
-      {15, 1776.82},
-      {16, 0.},
-      
-      //gauge bosons
-      {22, 0.},
-      {23, 91.1876},
-      {24, 80.385},
-      
-      //mesons
-      {111, .13957018},
-      {211, .1349766},
-      {221, .547862},
-      {311, .497614},
-      {321, .493677},
-      
-      //baryons
-      {2112, .939565379},
-      {2212, .938272046},
-    };
-
-
   std::string inFileName_;
   std::ifstream inFile_;
   bool endOfFile_ = false;
